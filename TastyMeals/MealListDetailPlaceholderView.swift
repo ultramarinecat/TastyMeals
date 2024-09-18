63863d0c357d7dd210440801a5512ce14e9591ca
ultramarinecat <ultramarinecat@fastmail.com>
2024-09-18 17:28:29 -0400

    Remove unnecessary constant declaration

--- TastyMeals/MealListDetailPlaceholderView.swift
+++ TastyMeals/MealListDetailPlaceholderView.swift
@@ -8,11 +8,10 @@ import SwiftUI
 /// Meal list detail placeholder view.
 struct MealListDetailPlaceholderView: View {
     private static let backgroundAccentColorOpacity = 0.2
-    private let mealSelectionPlaceholder = String(localized: "Please select a tasty dessert 🍰", comment: "Meal selection placeholder.")
 
     /// The content and behavior of the view.
     var body: some View {
-        Text(mealSelectionPlaceholder)
+        Text(String(localized: "Please select a tasty dessert 🍰", comment: "Meal selection placeholder."))
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.accentColor.opacity(MealListDetailPlaceholderView.backgroundAccentColorOpacity))
     }
